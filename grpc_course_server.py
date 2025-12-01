56e5a5e5c27a3169a443ba61ed5eb22d2d755d6e
Denis Bulygin <k49obron@gmail.com>
2025-12-01 21:44:47 +0400

    practic gRPC

---
+++ grpc_course_server.py
@@ -0,0 +1,31 @@
+from concurrent import futures
+
+import grpc
+
+import course_service_pb2
+import course_service_pb2_grpc
+
+
+class CourseServiceServicer(course_service_pb2_grpc.CourseServiceServicer):
+
+    def GetCourse(self, request, context):
+        title = "Автотесты API"
+        description = "Будем изучать написание API автотестов"
+        print(f"Получен запрос к методу GetCourses о курсе {request.course_id}")
+
+        return course_service_pb2.GetCourseResponse(
+            course_id=request.course_id,
+            title=title,
+            description=description
+        )
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    course_service_pb2_grpc.add_CourseServiceServicer_to_server(CourseServiceServicer(), server)
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    print("Сервер gRPC успешно запущен на порту 50051")
+    server.wait_for_termination()
+
+if __name__ == "__main__":
+    serve()

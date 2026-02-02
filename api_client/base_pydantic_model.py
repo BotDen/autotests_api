0ed073bbb5865c55da54daecec9a8faa85aad252
BotDen <BotDen@users.noreply.github.com>
2026-02-02 09:41:14 +0000

    deploy: 0f9233699722f6ab3b7091bb849feff82853f913

--- api_client/base_pydantic_model.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, ConfigDict
-
-
-class BasePydanticModel(BaseModel):
-    """
-    Базовая модель для генерации моделей pydantic в проекте
-    """
-    model_config = ConfigDict(populate_by_name=True)
